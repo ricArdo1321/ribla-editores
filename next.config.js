ae4e817d0add4337cebf9e4299f9a48fb3d2dd58
Ricardo <sqsricardo@gmail.com>
2025-12-09 13:21:40 -0300

    chore: configure @cloudflare/next-on-pages for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true, // Required for Cloudflare Pages
         remotePatterns: [
             {
                 protocol: 'https',
